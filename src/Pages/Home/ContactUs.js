13df11ccc01371eeb1ea41ed3ff17c5ec3a45e1a
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-10 21:08:43 +0600

    footer added

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -5,10 +5,10 @@ import appointment from './../../assets/images/appointment.png'
 
 const ContactUs = () => {
     return (
-        <section style={{ background: `url(${appointment})` }}>
+        <section style={{ background: `url(${appointment})` }} className="p-6">
             <TItle>Contact Us</TItle>
             <h2 className='text-3xl  text-white text-center'>Stay connected with us</h2>
-            <form action="" className='flex flex-col gap-4 md:w-2/4 mx-auto'>
+            <form action="" className='flex flex-col gap-4 md:w-2/4 mx-auto mt-4'>
                 <input type="text" placeholder='Email Address' className='p-4 rounded-lg' />
                 <input type="text" placeholder='Subject' className='p-4 rounded-lg' />
                 <textarea type="text" placeholder='Your message' className='p-4 rounded-lg' />
--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,29 +1,41 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import footer from './../../assets/images/footer.png'
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear()
     return (
-        <footer class="footer p-10 bg-neutral text-neutral-content">
-            <div>
-                <span class="footer-title">Services</span>
-                <a class="link link-hover">Branding</a>
-                <a class="link link-hover">Design</a>
-                <a class="link link-hover">Marketing</a>
-                <a class="link link-hover">Advertisement</a>
+        <footer style={{
+            background: `url(${footer})`,
+            backgroundSize: 'cover'
+        }} className=" p-10  text-accent">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to='\' className="link link-hover">Branding</Link>
+                    <Link to='\' className="link link-hover">Design</Link>
+                    <Link to='\' className="link link-hover">Marketing</Link>
+                    <Link to='\' className="link link-hover" > Advertisement</Link >
+                </div >
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to='\' className="link link-hover">About us</Link>
+                    <Link to='\' className="link link-hover" > Contact</Link >
+                    <Link to='\' className="link link-hover" > Jobs</Link >
+                    <Link to='\' className="link link-hover" > Press kit</Link >
+                </div >
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to='\' className="link link-hover">Terms of use</Link>
+                    <Link to='\' className="link link-hover" > Privacy policy</Link >
+                    <Link to='\' className="link link-hover" > Cookie policy</Link >
+                </div >
+
+            </div >
+            <div className='text-center my-10'>
+                <p>Copyright © {year} - All right reserved by ACME Industries Ltd</p>
             </div>
-            <div>
-                <span class="footer-title">Company</span>
-                <a class="link link-hover">About us</a>
-                <a class="link link-hover">Contact</a>
-                <a class="link link-hover">Jobs</a>
-                <a class="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span class="footer-title">Legal</span>
-                <a class="link link-hover">Terms of use</a>
-                <a class="link link-hover">Privacy policy</a>
-                <a class="link link-hover">Cookie policy</a>
-            </div>
-        </footer>
+        </footer >
     );
 };
 

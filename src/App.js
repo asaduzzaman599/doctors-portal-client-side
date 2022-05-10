e08cd37b032c1e65ec4be1e6a82110a2b33240ad
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-05-10 19:08:34 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
